5204a013626250ab6789dc17253bbe8247737b4b
Daven Hughes <daven.hughes@gmail.com>
2026-05-09 21:21:58 -0400

    Delete unused render result views

--- lib/src/Curve/Rasterization/RenderResult.h
+++ lib/src/Curve/Rasterization/RenderResult.h
@@ -12,32 +12,6 @@
 #include "../../Obj/ColorPoint.h"
 
 namespace Rasterization {
-    class WaveformView {
-    public:
-        WaveformView() = default;
-
-        explicit WaveformView(const WaveformBuffers& waveform) :
-                waveform(&waveform) {
-        }
-
-        Buffer<float> x() const { return waveform != nullptr ? waveform->waveX : Buffer<float>(); }
-        Buffer<float> y() const { return waveform != nullptr ? waveform->waveY : Buffer<float>(); }
-        Buffer<float> diffX() const { return waveform != nullptr ? waveform->diffX : Buffer<float>(); }
-        Buffer<float> slope() const { return waveform != nullptr ? waveform->slope : Buffer<float>(); }
-        Buffer<float> area() const { return waveform != nullptr ? waveform->area : Buffer<float>(); }
-
-        int zeroIndex() const { return waveform != nullptr ? waveform->zeroIndex : 0; }
-        int oneIndex() const { return waveform != nullptr ? waveform->oneIndex : 0; }
-        bool empty() const { return waveform == nullptr || waveform->waveX.empty(); }
-        const WaveformBuffers& buffers() const {
-            static WaveformBuffers empty;
-            return waveform != nullptr ? *waveform : empty;
-        }
-
-    private:
-        const WaveformBuffers* waveform {};
-    };
-
     class SamplerView {
     public:
         SamplerView() = default;
@@ -88,48 +62,6 @@ namespace Rasterization {
         const bool* sampleable {};
     };
 
-    class SnapshotView {
-    public:
-        SnapshotView() = default;
-
-        SnapshotView(
-                    const WaveformBuffers& waveform
-                ,   const std::vector<Intercept>& intercepts
-                ,   const std::vector<Curve>& curves
-                ,   const std::vector<ColorPoint>& colorPoints) :
-                waveform(&waveform)
-            ,   intercepts(&intercepts)
-            ,   curves(&curves)
-            ,   colorPoints(&colorPoints) {
-        }
-
-        WaveformView waveformView() const { return waveform != nullptr ? WaveformView(*waveform) : WaveformView(); }
-        const std::vector<Intercept>& getIntercepts() const { return intercepts != nullptr ? *intercepts : emptyIntercepts(); }
-        const std::vector<Curve>& getCurves() const { return curves != nullptr ? *curves : emptyCurves(); }
-        const std::vector<ColorPoint>& getColorPoints() const { return colorPoints != nullptr ? *colorPoints : emptyColorPoints(); }
-
-    private:
-        static const std::vector<Intercept>& emptyIntercepts() {
-            static std::vector<Intercept> empty;
-            return empty;
-        }
-
-        static const std::vector<Curve>& emptyCurves() {
-            static std::vector<Curve> empty;
-            return empty;
-        }
-
-        static const std::vector<ColorPoint>& emptyColorPoints() {
-            static std::vector<ColorPoint> empty;
-            return empty;
-        }
-
-        const WaveformBuffers* waveform {};
-        const std::vector<Intercept>* intercepts {};
-        const std::vector<Curve>* curves {};
-        const std::vector<ColorPoint>* colorPoints {};
-    };
-
     struct RenderResult {
         std::vector<Intercept> intercepts;
         std::vector<Intercept> frontPadding;
@@ -159,8 +91,6 @@ namespace Rasterization {
             needsResorting = false;
         }
 
-        WaveformView waveformView() const { return WaveformView(waveform); }
         SamplerView sampler() const { return SamplerView(waveform, &sampleable); }
-        SnapshotView snapshot() const { return SnapshotView(waveform, intercepts, curves, colorPoints); }
     };
 }
