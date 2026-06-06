8f3b35fb77184ec029faf4976b03f7b6c902e027
Daven Hughes <daven.hughes@gmail.com>
2026-06-06 00:35:13 -0300

    Split Trimesh panel bridge ownership

---
+++ cycle-v2/src/Nodes/Trimesh/TrimeshPanelEnvironment.cpp
@@ -0,0 +1,60 @@
+#include "TrimeshPanelEnvironment.h"
+
+#include <App/AppConstants.h>
+#include <App/MeshLibrary.h>
+#include <App/Settings.h>
+#include <Curve/Mesh/Vertex.h>
+#include <UI/MiscGraphics.h>
+
+namespace CycleV2 {
+
+TrimeshPanelEnvironment::TrimeshPanelEnvironment() :
+        console(&repo) {
+    repo.add(new AppConstants(&repo));
+    repo.add(new MiscGraphics(&repo));
+    repo.add(new Settings(&repo));
+    repo.add(new MeshLibrary(&repo));
+
+    auto& constants = repo.get<AppConstants>("AppConstants");
+    constants.setConstant(Constants::FontFace, String("Verdana"));
+
+    repo.get<MiscGraphics>("MiscGraphics").init();
+
+    auto& settings = repo.get<Settings>("Settings");
+    settings.initialiseSettings();
+    settings.createPropertiesFile(
+            File::getSpecialLocation(File::tempDirectory)
+                    .getChildFile("cycle-v2-trimesh-bridge-settings.xml")
+                    .getFullPathName());
+    settings.getGlobalSetting(AppSettings::DrawScales) = false;
+    repo.setConsole(&console);
+    repo.setMorphPositioner(&morphPositioner);
+}
+
+void TrimeshPanelEnvironment::setMorphPosition(
+        const MorphPosition& position,
+        int primaryAxis) {
+    morphPositioner.setPosition(position, primaryAxis);
+}
+
+TrimeshPanelEnvironment::NullConsole::NullConsole(SingletonRepo* repo) :
+        IConsole(repo, "CycleV2TrimeshNullConsole") {
+}
+
+void TrimeshPanelEnvironment::NodeMorphPositioner::setPosition(
+        const MorphPosition& position,
+        int primaryAxis) {
+    morph = position;
+    primaryDimension = primaryAxis;
+}
+
+float TrimeshPanelEnvironment::NodeMorphPositioner::getValue(int dim) {
+    switch (dim) {
+        case Vertex::Time: return morph.time.getCurrentValue();
+        case Vertex::Red:  return morph.red.getCurrentValue();
+        case Vertex::Blue: return morph.blue.getCurrentValue();
+        default:           return 0.f;
+    }
+}
+
+}
---
+++ cycle-v2/src/Nodes/Trimesh/TrimeshPanelHosts.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <UI/Panels/Panel.h>
+#include <UI/Panels/PanelHostContext.h>
+
+#include <functional>
+#include <memory>
+
+class CommonGL;
+class GLPanelRenderer;
+
+namespace CycleV2 {
+
+class TrimeshPanel2D;
+class TrimeshPanel3D;
+class TrimeshInteractor2D;
+class TrimeshInteractor3D;
+
+class TrimeshPanelHosts {
+public:
+    TrimeshPanelHosts(
+            TrimeshPanel2D& panel2D,
+            TrimeshPanel3D& panel3D,
+            TrimeshInteractor2D& interactor2D,
+            TrimeshInteractor3D& interactor3D);
+    ~TrimeshPanelHosts();
+
+    Component* getPanel3DHostComponent();
+    Component* getPanel3DHostComponentIfCreated();
+    Component* getPanel2DHostComponent();
+    Component* getPanel2DHostComponentIfCreated();
+
+    void setCallbacks(
+            std::function<void()> repaintCallback,
+            std::function<void(const MouseCursor&)> cursorCallback,
+            std::function<void(Point<float>)> hoverCallback);
+    void initialiseSharedGlResources();
+    void releaseSharedGlResources();
+    void renderPanel3D(Rectangle<float> bounds, float scaleFactor);
+    void renderPanel2D(Rectangle<float> bounds, float scaleFactor);
+
+    bool isPanel3DHostInitialised() const { return panel3DHostInitialised; }
+    bool isPanel2DHostInitialised() const { return panel2DHostInitialised; }
+
+private:
+    class PanelHostComponent;
+
+    PanelHostCallbacks createPanelHostCallbacks();
+    void initialisePanel3DHost();
+    void initialisePanel2DHost();
+    void updatePanelHostPeers();
+    void renderPanel(Panel& panel, Rectangle<float> bounds, float scaleFactor);
+
+    TrimeshPanel2D& panel2D;
+    TrimeshPanel3D& panel3D;
+    TrimeshInteractor2D& interactor2D;
+    TrimeshInteractor3D& interactor3D;
+    std::unique_ptr<PanelHostComponent> panel2DHost;
+    std::unique_ptr<PanelHostComponent> panel3DHost;
+    std::unique_ptr<GLPanelRenderer> panel2DRenderer;
+    std::unique_ptr<GLPanelRenderer> panel3DRenderer;
+    CommonGL* panel2DGfx {};
+    CommonGL* panel3DGfx {};
+    std::function<void()> panelHostRepaintCallback;
+    std::function<void(const MouseCursor&)> panelHostCursorCallback;
+    std::function<void(Point<float>)> panelHostHoverCallback;
+    bool panel3DHostInitialised {};
+    bool panel2DHostInitialised {};
+    bool sharedGlResourcesInitialised {};
+};
+
+}
